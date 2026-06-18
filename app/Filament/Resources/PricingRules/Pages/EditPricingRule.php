ab3d330ce3f4b6a0bf9b20a4fd0a2c334393f007
ivanpavlov-boop <ivan.pavlov@mycomputer.bg>
2026-06-18 13:07:06 +0300

    Fix rule edit save redirects
    Redirect Pricing Rule and Supplier Exclusion Rule edit saves back to their list pages.

--- app/Filament/Resources/PricingRules/Pages/EditPricingRule.php
+++ app/Filament/Resources/PricingRules/Pages/EditPricingRule.php
@@ -16,4 +16,9 @@ class EditPricingRule extends EditRecord
             DeleteAction::make(),
         ];
     }
+
+    protected function getRedirectUrl(): ?string
+    {
+        return $this->getResourceUrl();
+    }
 }
